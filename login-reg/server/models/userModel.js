3aba4e0c80f6aafe17ece83685dddc28c3cd800f
Aniket mandal <aniketmandal012@gmail.com>
2026-05-09 01:33:16 +0530

    implement MongoDB connection, add user model, and create .gitignore

---
+++ login-reg/server/models/userModel.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.userSchema({
+    name: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    verifyOtp: {type: String, default:''},
+    verifyOtpExpireAt: {type: Number, default:0},
+    isAccountVerified: {type: Boolean, default: true},
+    resetOtp: {type: String, default:''},
+    resetOtpExpireAt: {type: number, default:0},
+})
+
+const usermodel = mongoose.model('user', userSchema);
+export default usermodel
--- login-reg/server/server.js
+++ login-reg/server/server.js
@@ -2,13 +2,17 @@ import 'dotenv/config'
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import connectDB from './config/mongodb.js';
+
+
 
 const app = express();
 const port = process.env.PORT || 4000
 
+connectDB()
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({credentials: true}))
+app.use(cors({credentials: true}));
 
 app.get('/', (req, res)=>{
     res.send('API working')
