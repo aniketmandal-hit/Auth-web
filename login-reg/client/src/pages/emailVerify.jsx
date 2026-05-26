17dc38c845925723a3a9787bd6691c222e8f027d
Aniket mandal <aniketmandal012@gmail.com>
2026-05-26 23:54:39 +0530

    Add new page components for email verification, home, login, and reset password
    - Created emailVerify.jsx for email verification functionality
    - Created home.jsx for the home page
    - Created login.jsx for user login functionality
    - Created resetPassword.jsx for resetting user passwords

---
+++ login-reg/client/src/pages/emailVerify.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const emailVerify = () => {
+  return (
+    <div>
+      <h1>email verification page</h1>
+    </div>
+  )
+}
+
+export default emailVerify
---
+++ login-reg/client/src/pages/resetPassword.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const resetPassword = () => {
+  return (
+    <div>
+      <h1>reset password page</h1>
+    </div>
+  )
+}
+
+export default resetPassword
